4e688e89c34285be17a2c633ca06770e1755927b
devleandrodias <leandrodbdias@gmail.com>
2023-06-13 11:07:43 -0300

    feat: add ansible with terraform

--- iac/README.md
+++ iac/README.md
@@ -9,3 +9,5 @@
 > ssh -i "iac-alura.pem" ubuntu@ec2-52-54-227-251.compute-1.amazonaws.com
 
 > nohup busybox httpd -f -p 8080 &
+
+> ansible-playbook playbook.yml -u ubuntu --private-key iac-alura.pem -i hosts.yml
--- iac/main.tf
+++ iac/main.tf
@@ -18,8 +18,14 @@ resource "aws_instance" "app_server" {
   ami           = "ami-053b0d53c279acc90"
   instance_type = "t2.micro"
   key_name      = "iac-alura"
+  # user_data     = <<-EOF
+  #   #!/bin/bash
+  #   cd /home/ubuntu
+  #   echo "<h1>Wello Terraform</h1>" > index.html
+  #   nohup busybox httpd -f -p 8080 &
+  # EOF
 
   tags = {
-    Name = "PrimeiraInstancia"
+    Name = "TesteAws"
   }
 }
